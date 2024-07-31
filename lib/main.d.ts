6647ed8221ee9e8b27f7f0db303cb67b6d3c0aab
Johan Mouchet <mouchet.johan@gmail.com>
2024-07-31 16:08:33 +1000

    chore: remove types/ from repo and ensure they are generated with build:lib

---
+++ lib/main.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Insert [Word Break Opportunity tags](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/wbr) (`<wbr />`) to help an email address wrap in a human readable way.
+ *
+ * @example
+ * ```
+ * <PrettyEmailWrap>name@domain.com</PrettyEmailWrap> // name<wbr />@domain.<wbr />com
+ * ```
+ */
+export declare const PrettyEmailWrap: ({ children }: {
+    children: string;
+}) => import("react/jsx-runtime").JSX.Element | null;
+//# sourceMappingURL=main.d.ts.map
